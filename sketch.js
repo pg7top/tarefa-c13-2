f23bb66b42cd215b2035043865d7eb3315f5bb68
pg7top <111295676+pg7top@users.noreply.github.com>
2022-08-30 23:40:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+var garden,rabbit,apple,redL,orangeL;
+var gardenImg,rabbitImg,appleImg,redImg,orangeImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+ orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png"); 
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// mover o fundo
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//criar sprite do coelho
+rabbit = createSprite(180,340,30,30);
+rabbit.scale = 0.09;
+rabbit.addImage(rabbitImg);
+}
+function CreateApples(){
+  apple = createSprite(random(50,350),40,10,10)
+  apple.addImage(appleImg);
+  apple.scale= 0.07;
+  apple.velocityY= 3;
+  apple.lifetime = 150;
+
+
+
+ }
+ function CreateOrange(){
+  orangeL = createSprite(random(50,350),40,10,10)
+  orangeL.addImage(orangeImg);
+  orangeL.scale= 0.08;
+  orangeL.velocityY= 3;
+  orangeL.lifetime = 150;
+  
+  
+  
+   }
+   function Createred(){
+    redL = createSprite(random(50,350),40,10,10)
+    redL.addImage(redImg);
+    redL.scale= 0.06;
+    redL.velocityY= 3;
+    redL.lifetime = 150;
+    
+    
+    
+     }
+     
+
+function draw() {
+  background(0);
+
+
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  rabbit.x = World.mouseX;
+  
+  drawSprites();
+  var select_sprites = Math.round(random(1,3));
+
+     if (frameCount % 80 == 0) {
+       if (select_sprites == 1) {
+        CreateApples();
+
+
+       }else if (select_sprites == 2){
+        CreateOrange();
+
+       }else {
+        Createred();
+       }
+      }
+}
+ 
+      
+
+
+
+
+
+     
+     
